d0e1fd0ff8fd9eddc4e01c97b3074c930573abac
vadimhonghalo@gmail.com <vg@cee.com.ua>
2026-07-31 12:16:21 +0300

    refactor(bookings): isolate attendee coordination

--- apps/api/src/bookings/bookings.module.ts
+++ apps/api/src/bookings/bookings.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { NotificationsModule } from "../notifications/notifications.module";
+import { BookingAttendeesService } from "./booking-attendees.service";
 import { BookingImagesService } from "./booking-images.service";
 import { BookingsController } from "./bookings.controller";
 import { BookingsService } from "./bookings.service";
@@ -7,6 +8,6 @@ import { BookingsService } from "./bookings.service";
 @Module({
   imports: [NotificationsModule],
   controllers: [BookingsController],
-  providers: [BookingImagesService, BookingsService],
+  providers: [BookingAttendeesService, BookingImagesService, BookingsService],
 })
 export class BookingsModule {}
